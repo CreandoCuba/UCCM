928be94016dfd285d299e102633d5c6c6913a7fe
DarianApecheche <117931275+DarianApecheche@users.noreply.github.com>
2024-04-21 18:48:10 +0200

    updated

--- js/index.js
+++ js/index.js
@@ -18,57 +18,8 @@ window.sr = ScrollReveal();
         distance: '-400px',
     });
     
-    sr.reveal('#historia', {
-        duration: 2000,
-        origin: 'right',
-        distance: '-400px',
-    });
-
-    sr.reveal('#mision-vision', {
-        duration: 2000,
-        origin: 'left',
-        distance: '-400px',
-    });
-
     sr.reveal('#footer', {
         duration: 3000,
         origin: 'top',
         distance: '-100px',
     });
-
-    sr.reveal('.f1',{
-        duration: 2000,
-        origin: 'right',
-        distance: '-400px',
-    })
-
-    sr.reveal('.f2',{
-        duration: 2000,
-        origin: 'left',
-        distance: '-400px',
-    })
-
-    
-    sr.reveal('.r1',{
-        duration: 2000,
-        origin: 'right',
-        distance: '-400px',
-    })
-
-    sr.reveal('.r2',{
-        duration: 2000,
-        origin: 'right',
-        distance: '-200px',
-    })
-
-    sr.reveal('.r3',{
-        duration: 2000,
-        origin: 'left',
-        distance: '-200px',
-    })
-
-    sr.reveal('.r4',{
-        duration: 2000,
-        origin: 'left',
-        distance: '-400px',
-    })
